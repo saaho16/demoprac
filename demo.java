5c2f81c2f852e8d372ce2889939a7bdb369e6562
Kumar <tonystarkironman1696@gmail.com>
2024-02-21 16:47:17 -0500

    initial commit

---
+++ demo.java
@@ -0,0 +1,10 @@
+public class demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! Welcome to Food");
+        
+    }
+}//https://github.com/saaho16/demoprac
+
+//hRfHgva72R2aKr8
+//https://github.com/saaho16/demoprac.git
